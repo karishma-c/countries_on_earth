80fca8c0fe6a543c0dfaa8860fd77f509ac00e90
karishma-c <karishmadgct@gmail.com>
2023-10-28 15:21:08 +0530

    changed close icon and removed card css in modal

--- src/components/CardComponent/CardComponent.js
+++ src/components/CardComponent/CardComponent.js
@@ -24,7 +24,11 @@ const CardComponent = ({ countryDetails }) => {
                 
                     <Modal>
                         <span className="closeButton" onClick={closeModal}>
-                            x
+                            <svg id="closeIcon" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x" width="26" height="26" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <path d="M18 6l-12 12" />
+                                <path d="M6 6l12 12" />
+                            </svg>
                         </span>
                         <div className="modalCard" >
                             
